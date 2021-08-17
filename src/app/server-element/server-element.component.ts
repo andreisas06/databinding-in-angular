db513b30d2d76a7d720dcdafe33b31084bb3ebad
Andrei Sas <andreisas007@gmail.com>
2021-08-17 18:31:56 +0200

    learning about encapsulation

--- src/app/server-element/server-element.component.ts
+++ src/app/server-element/server-element.component.ts
@@ -1,12 +1,13 @@
-import { Component, Input, OnInit } from "@angular/core";
+import { Component, Input, OnInit, ViewEncapsulation } from "@angular/core";
 
 @Component({
   selector: "app-server-element",
   templateUrl: "./server-element.component.html",
   styleUrls: ["./server-element.component.css"],
+  encapsulation: ViewEncapsulation.Emulated, //did not have to add this line since it is default value, only for learning purposes
 })
 export class ServerElementComponent implements OnInit {
-  @Input('srvElement') element: { type: string; name: string; content: string };
+  @Input("srvElement") element: { type: string; name: string; content: string };
 
   constructor() {}
 
